6dac45522c9b84694738f0649bb96e14f0bb040d
Seajay <cjmcerezo2021@plm.edu.ph>
2024-10-06 01:39:22 +0800

    update

--- scheduling/models.py
+++ scheduling/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 
 class Appointment(models.Model):
-    # Appointment Date - Date Field
-    appointment_date = models.DateField()
-
-    # Appointment Time - Time Field
-    appointment_time = models.TimeField()
-
-    # Client Name - CharField for the name
+    appointment_date = models.CharField(max_length=100)
+    appointment_time = models.CharField(max_length=100)
     client_name = models.CharField(max_length=100)
-
-    # Contact Number - CharField for phone numbers
     contact_number = models.CharField(max_length=15)
-
-    # Employee Availability - Nullable field
     employee_availability = models.CharField(max_length=100, blank=True, null=True)
 
     # Service Catalog Dropdown (Brazilian, Haircolor, Haircut)
--- scheduling/views.py
+++ scheduling/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import AppointmentForm
 
 def create_appointment(request):
-    if request.method == 'POST':
-        form = AppointmentForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('success')  # You can define where to redirect after saving the form
-    else:
-        form = AppointmentForm()
-    
+    form = AppointmentForm()
+    context = {
+            'form': form,
+        }
     return render(request, 'scheduling/appointment_form.html', {'form': form})
